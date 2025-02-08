eba70b13cd01c303ef262969943fc7c994d3cc59
Stephen Tim <571268@qq.com>
2025-02-08 10:56:40 +0800

    Initial Commit

---
+++ Reversi/ReversiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReversiApp.swift
+//  Reversi
+//
+//  Created by Stephen Tim on 2025/2/8.
+//
+
+import SwiftUI
+
+@main
+struct ReversiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
